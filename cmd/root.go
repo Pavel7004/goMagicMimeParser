19ff64d5562071db66c8dbb139274c5085d399a3
Kovalev Pavel <kovalev5690@gmail.com>
2023-03-13 13:09:12 +0300

    cmd/root: open/close file

--- cmd/root.go
+++ cmd/root.go
@@ -6,25 +6,21 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+
+	"github.com/Pavel7004/goMimeMagic/pkg/magic"
 )
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "goMimeMagic",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Use:   "magic",
+	Short: "Utility that parses MIME types binary file",
+	Long: `goMimeMagic is a utility that reads magic binary file
+to get binary signatures for filetypes.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+Example: magic
+This will print all types and their signatures.`,
+	Run: listAll,
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -33,13 +29,12 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.goMimeMagic.yaml)")
+func listAll(cmd *cobra.Command, args []string) {
+	r := magic.NewMagicReader()
 
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	cobra.CheckErr(r.Open())
+	defer cobra.CheckErr(r.Close())
 }
